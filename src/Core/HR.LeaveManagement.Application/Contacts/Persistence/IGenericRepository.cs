125d3e5e6b9f84c8d8c7116aed5338106bf013f5
Pheeraphat Prisan <165787650+newpheeraphatflowaccount@users.noreply.github.com>
2025-10-04 03:13:01 +0700

    (feat): Add Application project and repository interfaces
    Created the HR.LeaveManagement.Application project with interfaces for generic repository, leave allocation, leave request, and leave type repositories. Updated the solution file to include the new project and set up project references to the domain layer.

---
+++ src/Core/HR.LeaveManagement.Application/Contacts/Persistence/IGenericRepository.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HR.LeaveManagement.Application.Contacts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+  Task<T> GetAsync();
+  Task<T> GetByIdAsync(int id);
+  Task<T> CreateAsync(T entity);
+  Task<T> UpdateAsync(T entity);
+  Task<T> DeleteAsync(T entity);
+}
